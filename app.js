7486f84005dc74321e090ceacee274b6004431f0
Maria <114153517+eyesmaybeyes@users.noreply.github.com>
2023-03-28 04:38:54 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+let game = document.querySelector('.game');
+
+let result = document.querySelector('.result');
+
+let btnGame = document.querySelector('.new-game');
+
+let fields = document.querySelectorAll('.field');
+
+let step = false;
+
+let circle = `<svg class="circle">
+<circle r="45" cx="58" cy="58" stroke="blue" stroke-width="10" fill="none" stroke-linecap="round">
+</svg>`
+
+let cross = `<svg class="cross">
+<line class="first" x1="15" y1="15" x2="100" y2="100" stroke="red" stroke-width="10" stroke-linecap="round"></line>
+<line class="second" x1="100" y1="15" x2="15" y2="100" stroke="red" stroke-width="10" stroke-linecap="round"></line>
+</svg>`
+
+function stepCross() {
+
+}
+
+function stepZero() {
+
+
+}
